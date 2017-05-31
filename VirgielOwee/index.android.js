061e2b892cfbbe09777057830a079cf84d42ee81
pietertolsma <epieter.tolsma@gmail.com>
2017-05-31 21:34:47 +0200

    Add skeleton

---
+++ VirgielOwee/index.android.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class VirgielOwee extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+AppRegistry.registerComponent('VirgielOwee', () => VirgielOwee );
