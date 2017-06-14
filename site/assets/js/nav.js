5899b999ddf3594f492c745c3efb9118b1384fe6
pietertolsma <epieter.tolsma@gmail.com>
2017-06-15 01:10:33 +0200

    Add circle

--- site/assets/js/nav.js
+++ site/assets/js/nav.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
     offsetX = x - $(window).width() / 2;
     offsetY = y - $(window).height() / 2;
 
+    if (Math.abs(offsetX) < 100 && Math.abs(offsetY) < 100) {
+      $(".compass").css({
+        'background' : 'black'
+      });
+      $("#compass").hide();
+    } else {
+      $(".compass").css({
+        'background' : 'white'
+      });
+      $("#compass").show();
+    }
+
     angle = 180 * Math.atan(offsetX / offsetY) / Math.PI;
     if (offsetY > 0) {
       angle += 180;
