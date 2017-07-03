a6bad841b2cd154de3520cd1495fd4685421bb3d
T.T.Naber <T.T.Naber@student.tudelft.nl>
2017-07-04 00:46:37 +0200

    Mobile pages updated for home, sport and vereniging

---
+++ site/m/assets/js/pages.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+  curView = 0
+
+  $("#south-link").on("click", function(event){
+    if (curView == 0) {
+      $(".mobile-container").css({
+        'left' : '-100%'
+      });
+      $(".navbar").css({
+        'top' : '0%'
+      });
+      curView = 3
+    }
+  });
+
+  $(".goBackButton").on("click", function(event){
+    console.log("Hi")
+
+    if (curView != 0) {
+      $(".mobile-container").css({
+        'left' : '0%'
+      });
+      $(".navbar").css({
+        'top' : '-10%'
+      });
+      curView = 0
+    }
+  })
+
+});
