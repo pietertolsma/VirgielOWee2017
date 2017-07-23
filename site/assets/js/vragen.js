5caa92e695100ba8fbd4c8870d849dbeaab32dc9
J.l.Buijnsters <J.L.Buijnsters@student.tudelft.nl>
2017-07-24 00:11:05 +0200

    Makkelijker vragen uitklappen

--- site/assets/js/vragen.js
+++ site/assets/js/vragen.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   console.log("Vragen connected.")
 
-  $('h2').on('click', function() {
+  $('.QHeader').on('click', function() {
+    console.log('CLICK');
     if($(this).next().is(":visible")) {
-      $(this).prev().css({'transform' : 'rotate('+ 0 +'deg)' , 'transition' : '200ms linear all' });
+      $(this).children().first().css({'transform' : 'rotate('+ 0 +'deg)' , 'transition' : '200ms linear all' });
     } else {
-      $(this).prev().css({'transform' : 'rotate('+ 90 +'deg)' , 'transition' : '200ms linear all' });
+      $(this).children().first().css({'transform' : 'rotate('+ 90 +'deg)' , 'transition' : '200ms linear all' });
     }
 
     $(this).next().slideToggle();
